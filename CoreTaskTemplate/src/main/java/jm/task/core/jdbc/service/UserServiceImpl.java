e8cf46ce89e85f3f757775861e937ca3894d0415
vetir <63797640+vetir@users.noreply.github.com>
2020-09-28 17:55:21 +0300

    Update UserServiceImpl.java

--- CoreTaskTemplate/src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ CoreTaskTemplate/src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
-    private UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
+    private final UserDao userDao = new UserDaoJDBCImpl();
 
     public void createUsersTable() {
         userDao.createUsersTable();
