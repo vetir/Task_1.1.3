cb13102ce2137a06e781d1902d8cf22eb35245c2
vetir@mail.ru <vetir@mail.ru>
2020-10-12 13:47:25 +0300

    Решение задачи 2.1.2

---
+++ spring-helloworld-master/spring-helloworld-master/src/main/java/AppConfig.java
@@ -0,0 +1,23 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Configuration
+public class AppConfig {
+ 
+    @Bean(name="helloworld")
+    public HelloWorld getHelloWorld() {
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.setMessage("Hello World!");
+        return helloWorld;
+    }
+
+    @Bean(name="cat")
+    @Scope("prototype")
+    public Cat getCatName() {
+        Cat cat = new Cat();
+        cat.setCatName("Tuzik");
+        return cat;
+    }
+}
---
+++ spring-helloworld-master/spring-helloworld-master/src/main/java/Cat.java
@@ -0,0 +1,17 @@
+public class Cat {
+
+    private String name;
+
+    /*public Cat() {
+
+    }*/
+
+    public String getCatName() {
+        return name;
+    }
+
+    public void setCatName(String name) {
+        this.name = name;
+    }
+
+}
