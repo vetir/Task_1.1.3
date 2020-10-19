b5d5ca2b421e209487924e71408fb2902c98e851
vetir@mail.ru <vetir@mail.ru>
2020-10-19 19:17:09 +0300

    solution of task №2.1.3

---
+++ spring-beans-master/spring-beans-master/src/main/java/app/model/AnimalsCage.java
@@ -0,0 +1,33 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AnimalsCage {
+
+    private final Animal animal;
+
+    public final Timer timer;
+
+    @Autowired
+    public AnimalsCage(@Qualifier("dog") Animal animal, Timer timer) {
+        this.animal = animal;
+        this.timer = timer;
+    }
+
+    public Timer getTimer() {
+        return timer;
+    }
+
+    @Autowired
+    public void whatAnimalSay() {
+        System.out.println("Say:");
+        System.out.println(animal.toString());
+        System.out.println("At:");
+        System.out.println(new Timer().getTime());
+        System.out.println("________________________");
+    }
+
+}
